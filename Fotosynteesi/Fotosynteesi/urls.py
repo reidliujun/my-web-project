1d63856a2e44bc68701584a05748464e126f32a2
Erik Enomaa <e.enomaa@umiami.edu>
2014-02-14 01:12:49 +0200

    Fixed urls.py to properly handle album titles.
    Slugs or otherwise.

--- Fotosynteesi/Fotosynteesi/urls.py
+++ Fotosynteesi/Fotosynteesi/urls.py
@@ -23,11 +23,11 @@ urlpatterns = patterns('',
 
     url(r'^album_form/', 'website.views.album_form', name='album_form'),
 
-    url(r'^public/(?P<albumurl>\w+)/$', 'website.views.publicalbum', name='publicalbum'),
-    url(r'^public/(?P<albumurl>\w+)/(?P<pagenumber>\d{1,3})/$', 'website.views.publicpage', name='publicpage'),
+    url(r'^public/(?P<albumurl>[-\w]+)/$', 'website.views.publicalbum', name='publicalbum'),
+    url(r'^public/(?P<albumurl>[-\w]+)/(?P<pagenumber>\d{1,3})/$', 'website.views.publicpage', name='publicpage'),
 
 
-    url(r'^album/(?P<albumtitle>\w+)/page/(?P<pagenumber>\d{1,3})/layout/(?P<layoutstyle>\d{1})/$',
+    url(r'^album/(?P<albumtitle>[-\w]+)/page/(?P<pagenumber>\d{1,3})/layout/(?P<layoutstyle>\d{1})/$',
         'website.views.photoadd', name='photoadd'),
 
     # (Album List View):
@@ -40,35 +40,35 @@ urlpatterns = patterns('',
 
     # (Single Album View:)
     #  Available upon clicking a specific album in (Album List View)
-    url(r'^album/(?P<album_title>\w+)/$',
+    url(r'^album/(?P<title_slug>[-\w]+)/$',
         'website.views.single_album_view', name='Single Album View'),
 
     # (Single Page View):
     #  Available upon clicking a page in album view.
     # FIXME: There's currently nothing useful to where this leads.
-    url(r'^album/(?P<album_title>\w+)/page/(?P<page_number>\d{1,3})/$',
+    url(r'^album/(?P<title_slug>[-\w]+)/page/(?P<page_number>\d{1,3})/$',
         'website.views.single_page_view', name='Single Page View'),
 
     # (Add New Page):
-    url(r'^album/(?P<album_title>\w+)/(?P<action>)\w+/$',
+    url(r'^album/(?P<title_slug>[-\w]+)/new_page/$',
         'website.views.add_new_page'),
 
     # (Layout Selection View):
     #  Available upon clicking "add page" in (Single album view).
     # TODO: Should be available from (Page view), as well.
-    url(r'^album/(?P<albumtitle>\w+)/page/(?P<pagenumber>\d{1,3})/layout/$',
+    url(r'^album/(?P<albumtitle>[-\w]+)/page/(?P<pagenumber>\d{1,3})/layout/$',
         'website.views.select_page_layout', name='Layout Selection View'),
 
-    url(r'^album/(?P<albumtitle>\w+)/page/(?P<pagenumber>\d{1,3})/delete/', 'website.views.page_delete', name='page_delete'),
+    url(r'^album/(?P<albumtitle>[-\w]+)/page/(?P<pagenumber>\d{1,3})/delete/', 'website.views.page_delete', name='page_delete'),
 
-    url(r'^album/delete/(?P<albumtitle>\w+)', 'website.views.album_delete', name='album_delete'),
+    url(r'^album/delete/(?P<albumtitle>[-\w]+)', 'website.views.album_delete', name='album_delete'),
 
-    url(r'^album/(?P<albumtitle>\w+)/order/$', 'website.views.album_order', name='album_order'),
+    url(r'^album/(?P<albumtitle>[-\w]+)/order/$', 'website.views.album_order', name='album_order'),
 
-    url(r'^album/(?P<albumtitle>\w+)/submit/$', 'website.views.order_submit', name='order_submit'),
-    url(r'^album/(?P<albumtitle>\w+)/paysuccess$', 'website.views.paysuccess', name='paysuccess'),
-    url(r'^album/(?P<albumtitle>\w+)/paycancel$', 'website.views.paycancel', name='paycancel'),
-    url(r'^album/(?P<albumtitle>\w+)/payerror$', 'website.views.payerror', name='payerror'),
+    url(r'^album/(?P<albumtitle>[-\w]+)/submit/$', 'website.views.order_submit', name='order_submit'),
+    url(r'^album/(?P<albumtitle>[-\w]+)/paysuccess$', 'website.views.paysuccess', name='paysuccess'),
+    url(r'^album/(?P<albumtitle>[-\w]+)/paycancel$', 'website.views.paycancel', name='paycancel'),
+    url(r'^album/(?P<albumtitle>[-\w]+)/payerror$', 'website.views.payerror', name='payerror'),
 
     # url(r'^page/$', 'website.views.page', name='page'),
     url(r'^photo/$', 'website.views.photo', name='photo'),
@@ -76,7 +76,7 @@ urlpatterns = patterns('',
     url(r'^about/$', 'website.views.about', name='about'),
     url(r'^account/$', 'website.views.account', name='account'),
     # url(r'^$', RedirectView.as_view(url='/home/')),
-    url(r'^facebook_post/(?P<albumtitle>\w+)/$', 'website.views.facebook_post', name='facebook_post'),
+    url(r'^facebook_post/(?P<albumtitle>[-\w]+)/$', 'website.views.facebook_post', name='facebook_post'),
 
     # TODO: static.static() function not intended for deployment, read below
     # link to docs: https://docs.djangoproject.com/en/1.6/ref/urls/
